6a0607c79254b19adfdcd1580a81160d8bb5b171
jhamin0511 <jhamin0511@gmail.com>
2021-01-25 15:35:52 +0900

    feat: 화면

--- app/src/main/java/com/github/jhamin0511/britriseexample/MainActivity.kt
+++ app/src/main/java/com/github/jhamin0511/britriseexample/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.github.jhamin0511.britriseexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val cal = Calculate()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val oneText: TextView = findViewById(R.id.one)
+        val twoText: TextView = findViewById(R.id.two)
+        val typeButton: Button = findViewById(R.id.type)
+        val calButton: Button = findViewById(R.id.calculate)
+        val resultText: TextView = findViewById(R.id.result)
+
+        calButton.setOnClickListener {
+            val first = oneText.text.toString().toInt()
+            val second = twoText.text.toString().toInt()
+            val result = cal.result(first, second)
+
+            resultText.setText(result)
+        }
     }
+
 }
